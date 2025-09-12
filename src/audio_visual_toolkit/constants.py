e3fe8653a13dacdca4b25ad4928bccb8d9b7f75c
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-09-12 15:11:19 +0900

    chore: MS版の音素をアップデート

--- src/audio_visual_toolkit/constants.py
+++ src/audio_visual_toolkit/constants.py
@@ -1,9 +1,10 @@
-# MPEG4 Visemes (14+1 IDs)
-# https://visagetechnologies.com/uploads/2012/08/MPEG-4FBAOverview.pdf
-# https://developers.meta.com/horizon/documentation/native/audio-ovrlipsync-viseme-reference
 from typing import Literal, TypeAlias, get_args
 
 
+# MPEG4 Visemes (14+1 IDs)
+# https://visagetechnologies.com/uploads/2012/08/MPEG-4FBAOverview.pdf
+# https://developers.meta.com/horizon/documentation/native/audio-ovrlipsync-viseme-reference
+# https://beta.developers.meta.com/horizon/reference/unity/v78/class_o_v_r_face_expressions
 Mpeg4Visemes = Literal[
     "sil",
     "PP",
@@ -23,8 +24,33 @@ Mpeg4Visemes = Literal[
 ]
 MPEG4_VISEMES = list(get_args(Mpeg4Visemes))
 
-# Azure Visemes (22+1 IDs)は未対応。
+# https://learn.microsoft.com/en-us/previous-versions/windows/desktop/ms717289%28v%3Dvs.85%29
 # https://learn.microsoft.com/azure/ai-services/speech-service/how-to-speech-synthesis-viseme
+# https://chatgpt.com/share/68c37a00-fb28-8010-8a3f-ceef2e318689
+SpVisemeNickNames = Literal[
+    "sil",
+    "AX",
+    "AA",
+    "AO",
+    "E",
+    "ER",
+    "I",
+    "U",
+    "OW",
+    "AW",
+    "OY",
+    "AY",
+    "H",
+    "R",
+    "L",
+    "SZ",
+    "SHCH",
+    "TH",
+    "FV",
+    "DTN",
+    "KGN",
+    "PBM",
+]
 
 # OpenJTalkの日本語音素一覧
 # ずんずんPJマルチモーダルデータベースのlabelファイルがOpenJTalk準拠と思われるため、そのようにした
