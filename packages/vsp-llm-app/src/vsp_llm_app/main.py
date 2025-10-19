d36e3cae323306e4aa6fb4332645ce0e89d2c689
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-10-19 15:03:10 +0900

    feat: load cfg from checkpoint...

--- packages/vsp-llm-app/src/vsp_llm_app/main.py
+++ packages/vsp-llm-app/src/vsp_llm_app/main.py
@@ -2,10 +2,12 @@ import argparse
 import logging
 import os
 from transformers import AutoTokenizer
-
+from fairseq import checkpoint_utils
+from omegaconf import OmegaConf
 from vsp_llm.vsp_llm import avhubert_llm_seq2seq_cluster_count, VSPLLMConfig
+from vsp_llm.hubert_pretraining import AVHubertPretrainingConfig
 
-def main(video_path: str, w2v_path: str, llm_path: str):
+def main(video_path: str, model_path: str, w2v_path: str, llm_path: str):
     logging.basicConfig(
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
@@ -13,33 +15,29 @@ def main(video_path: str, w2v_path: str, llm_path: str):
     )
     tokenizer = AutoTokenizer.from_pretrained(llm_path)
 
-    # Default values: https://github.com/Sally-SH/VSP-LLM/blob/91c4f3d4418ac9cd0f6328f043de5e48e1d091b2/src/vsp_llm.py#L34
-    # Checkpoint values: uvx --python 3.10 --from 'git+https://github.com/xhiroga/audio-visual-toolkit#subdirectory=packages/fairseq-toolkit' dump --model-path $REPOS/VSP-LLM/checkpoints/checkpoint_finetune.pt
-    # Train values: https://github.com/Sally-SH/VSP-LLM/blob/91c4f3d4418ac9cd0f6328f043de5e48e1d091b2/src/conf/vsp-llm-433h-finetune.yaml#L73
-    cfg = VSPLLMConfig(
-        w2v_path=w2v_path,
-        llm_ckpt_path=llm_path,
-        apply_mask =False,
-        mask_selection= "static",
-        mask_length= 10,
-        mask_other= 0,
-        mask_prob= 0.75,
-        mask_channel_selection= "static",
-        mask_channel_length= 64,
-        mask_channel_other= 0,
-        mask_channel_prob= 0.5,
-        layerdrop= 0.1,
-        dropout= 0.0,
-        activation_dropout= 0.1,
-        attention_dropout= 0.0,
-        feature_grad_mult= 1.0,
-        encoder_embed_dim= 1024,
-        decoder_embed_dim= 4096,
-        freeze_finetune_updates= 18000,
-        # Should be same with w2v_args.task.normalize.
-        normalize=True,
+    # fairseq モデルをビルドするための cfg の構築には、次の通り複雑な手順が必要。
+    # なおこの手順は、ModelとConfigは最新・Checkpotinは古いという前提に基づくため、結局は場合による。
+    # 1. Dataclass から OmegaConf の設定を生成する。checkpointのcfgは最新版のキーを持たないことがある。
+    # 怠った場合、`omegaconf.errors.ConfigAttributeError: Key 'dropout_input' is not in struct`のようなエラーが発生
+    # 2. checkpoint の cfg をコンテナ(dict)に変換し、Modelのdataclassに存在するキーのみを抽出する。
+    # 怠った場合、`OmegaConf.merge`の際に`omegaconf.errors.ConfigKeyError: Key 'decoder_layers' not in 'VSPLLMConfig'
+    # 3. CLIから渡される値を設定する。
+    # 4. checkpoint からは復元できないにも関わらず、VSPLLMConfig では設定ツリー上の他の項目に依存するキーがある。解決されないまま使うとエラーなので手動で設定する。
+    # 怠った場合、`omegaconf.errors.ConfigKeyError: str interpolation key 'task.normalize' not found`
+    # 5. vsp-llm についてはモデルのみを構築するが、その内側で使う AVHuBERT は タスクごとビルドするので、関連設定かつローカルパスに依存する箇所を上書き。
+    state = checkpoint_utils.load_checkpoint_to_cpu(model_path)
+    base_cfg = state["cfg"]
+
+    structured: VSPLLMConfig = OmegaConf.structured(VSPLLMConfig)
+    OmegaConf.set_struct(structured, False)
+
+    model_cfg: VSPLLMConfig = OmegaConf.merge(
+        structured,
+        base_cfg.model,
+        {"w2v_path": w2v_path, "llm_ckpt_path": llm_path, "normalize": True, "data": "", "w2v_args": {"task": {"labels": []}}},
     )
-    model = avhubert_llm_seq2seq_cluster_count.build_model(cfg=cfg, task=None)
+    model = avhubert_llm_seq2seq_cluster_count.build_model(cfg=model_cfg, task=None)
+    model.eval()
 
     print(f"{model}")
 
@@ -48,7 +46,8 @@ def main(video_path: str, w2v_path: str, llm_path: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--video-path", required=True)
+    parser.add_argument("--model-path", required=True)
     parser.add_argument("--w2v-path", required=True)
     parser.add_argument("--llm-path", required=True)
     args = parser.parse_args()
-    main(video_path=args.video_path, llm_path=args.llm_path, w2v_path=args.w2v_path)
+    main(video_path=args.video_path, model_path=args.model_path, llm_path=args.llm_path, w2v_path=args.w2v_path)
